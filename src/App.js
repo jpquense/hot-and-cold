10e015377a1c56e8dbad92bb36f82cde83fc87f1
John P Quense <jpquense@gmail.com>
2018-04-14 12:18:15 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src='/images/fire-and-ice.jpg' className="App-logo" alt="fire and ice logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
